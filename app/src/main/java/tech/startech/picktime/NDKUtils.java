a0e9ae6b623b062e532121ce932b11800817d427
unknown <ekuaibao1@163.com>
2017-08-27 07:22:32 +0800

    add opencv lib

--- app/src/main/java/tech/startech/picktime/NDKUtils.java
+++ app/src/main/java/tech/startech/picktime/NDKUtils.java
@@ -1,5 +1,7 @@
 package tech.startech.picktime;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by jsb-hdp-0 on 2017/8/22.
  */
@@ -9,4 +11,7 @@ public class NDKUtils {
     public static native int[] reverse(int[] buf, int w, int h);    //取反
     public static native int[] blur(int[] buf, int w, int h);       //高斯模糊
     public static native int[] desColor(int[] beforeBuffer, int[] afterBuffer, int w, int h);   //颜色减淡
+    public static native void sketch(Bitmap bitmap);                 //canny边缘检测
+
+
 }
--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,39 +1,44 @@
-#LOCAL_PATH:=$(call my-dir)
-
-#include $(CLEAR_VARS)
-#OpenCV_INSTALL_MODULES := on
-#OpenCV_CAMERA_MODULES := off
-#OPENCV_LIB_TYPE :=STATIC
-
-#ifeq ("$(wildcard $(OPENCV_MK_PATH))","")
-#include $(LOCAL_PATH)/native/jni/OpenCV.mk
-#else
-#include $(OPENCV_MK_PATH)
-#endif
-#LOCAL_MODULE := blur
-#LOCAL_SRC_FILES := blur.c
-#LOCAL_LDLIBS +=  -lm -llog
-#include $(BUILD_SHARED_LIBRARY)
-
-
-
-
-
-
-LOCAL_PATH := $(call my-dir)
-
-# Create BitmapUtils library
+LOCAL_PATH:=$(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_LDLIBS    := -llog -ljnigraphics
 
-LOCAL_MODULE    := blur
-LOCAL_SRC_FILES := blur.c
+OpenCV_INSTALL_MODULES := on
+OpenCV_CAMERA_MODULES := off
+OPENCV_LIB_TYPE :=STATIC
 
-LOCAL_CFLAGS    =  -ffast-math -O3 -funroll-loops
+ifeq ("$(wildcard $(OPENCV_MK_PATH))","")
+    include $(LOCAL_PATH)/native/jni/OpenCV.mk
+else
+    include $(OPENCV_MK_PATH)
+endif
 
+
+LOCAL_MODULE := sketch
+LOCAL_SRC_FILES := sketch.cpp
+LOCAL_LDLIBS +=  -lm -llog
 include $(BUILD_SHARED_LIBRARY)
+#$(warning "the value of LOCAL_PATH is$(BUILD_SHARED_LIBRARY)")
+
+
+
+
+
+
+#LOCAL_PATH := $(call my-dir)
+
+# Create BitmapUtils library
+
+#include $(CLEAR_VARS)
+
+#LOCAL_LDLIBS    := -llog -ljnigraphics
+
+#LOCAL_MODULE    := sketch
+#LOCAL_SRC_FILES := sketch.cpp
+
+#LOCAL_CFLAGS    =  -ffast-math -O3 -funroll-loops
+
+#include $(BUILD_SHARED_LIBRARY)
 
 # Renderscript support library is not available for ARM before v7a currently
 ifneq ($(TARGET_ARCH_ABI),armeabi)
