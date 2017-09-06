f6fb9becd76579577929787812d3cba092355f82
diskrooms <diskrooms@gmail.com>
2017-09-06 23:05:05 +0800

    fix some bugs

--- app/src/main/java/tech/startech/picktime/CommonUtils.java
+++ app/src/main/java/tech/startech/picktime/CommonUtils.java
@@ -1,12 +1,16 @@
 package tech.startech.picktime;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.util.DisplayMetrics;
 import android.util.Size;
 import android.view.WindowManager;
 
 import com.apkfuns.logutils.LogUtils;
 
+import java.io.IOException;
 import java.util.Comparator;
 
 /**
@@ -115,5 +119,60 @@ public class CommonUtils {
                     (long) rhs.getWidth() * rhs.getHeight());
         }
 
+    }
+
+    /**
+     * 获取图片exif旋转角度信息
+     * @param path 图片路径
+     */
+    public static int getPicDegree(String path){
+        int degree = 0;
+        try {
+            // 从指定路径下读取图片，并获取其EXIF信息
+            ExifInterface exifInterface = new ExifInterface(path);
+            // 获取图片的旋转信息
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return degree;
+    }
+
+    /**
+     * 将图片按照某个角度进行旋转
+     * @param originBitmap     需要旋转的图片
+     * @param degree 旋转角度
+     * @return 旋转后的图片
+     */
+    public static Bitmap rotateBitmapDegree(Bitmap originBitmap, int degree) {
+        Bitmap newBitmap = null;
+        // 根据旋转角度，生成旋转矩阵
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        try {
+            // 将原始图片按照旋转矩阵进行旋转，并得到新的图片
+            newBitmap = Bitmap.createBitmap(originBitmap, 0, 0, originBitmap.getWidth(), originBitmap.getHeight(), matrix, true);
+        } catch (OutOfMemoryError e) {
+            //TODO
+        }
+        if (newBitmap == null) {
+            newBitmap = originBitmap;
+        }
+        if (originBitmap != newBitmap) {
+            originBitmap.recycle();
+        }
+        return newBitmap;
     }
 }
